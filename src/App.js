66d7f2033a37a5d60610417097ea3045e2a320ff
Gyasi <glcalhoun1123@yahoo.com>
2022-04-11 01:09:48 -0400

    initial commit including anime js svgs implementation

--- src/App.js
+++ src/App.js
@@ -12,6 +12,23 @@ function App() {
   var path = anime.path('#Box');
   return (
     <div className='main-container'>
+        <Anime
+          initial={
+            [
+              {
+                targets: ['.polygon', 'feTurbulence', 'feDisplacementMap'],
+                points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+                baseFrequency: 0,
+                scale: 1,
+                loop: true,
+                direction: 'alternate',
+                easing: 'easeInOutExpo'
+              }
+            ]
+          }
+        >
+
+               </Anime>
       <div className='flex-content-div'>
         <Anime
           initial={
