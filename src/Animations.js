52bcb2a4a3e13eb105c4778894f2175b68ae960f
Gyasi <glcalhoun1123@yahoo.com>
2022-04-11 01:07:43 -0400

    initial commit including anime js svgs implementation

---
+++ src/Animations.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import anime from 'animejs';
+import ReactAnime from 'react-animejs'
+
+function Animations() {
+  const { Anime, stagger } = ReactAnime
+  return (
+
+    <Anime>
+      initial={
+        
+      }
+    </Anime>
+  );
+}
+
+export default Animations;
